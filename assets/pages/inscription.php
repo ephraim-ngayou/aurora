14362bfcd16353976ea1f2719fc94848a32a10c9
Cuvillier--Couvreur Béryl <beryl.cuvillier@gmail.com>
2026-03-24 11:03:11 +0100

    ajout telephone

--- assets/pages/inscription.php
+++ assets/pages/inscription.php
@@ -2,39 +2,40 @@
 include '../includes/header.php'
 ?>
 
-    <div class="inscription">
-        <form action="#" method="post" class="sigin-form">
-            <label for="name">Nom</label>
-            <input type="name" id="name" placeholder="Votre Nom" required>
+<div class="inscription">
+    <form action="#" method="post" class="sigin-form">
+        <label for="name">Nom</label>
+        <input type="name" id="name" placeholder="Votre Nom" required>
 
-            <form action="#" method="post" class="sigin-form">
-                <label for="firstname">Prenom</label>
-                <input type="firstname" id="firstname" placeholder="Votre Prenom" required>
+        <label for="firstname">Prenom</label>
+        <input type="firstname" id="firstname" placeholder="Votre Prenom" required>
 
-                <form action="#" method="post" class="sigin-form">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" placeholder="Votre email" required>
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Votre email" required>
 
-                    <label for="password">Mot de passe</label>
-                    <input type="password" id="password" placeholder="Votre mot de passe" required>
+        <label for="phone">Numéro de téléphone</label>
+        <input type="tel" id="tel" placeholder="Votre numéro de téléphone" required>
 
-                    <button type="submit">Connexion</button>
-                    </section>
-    </div>
-    <script>
-        const toggle = document.getElementById("menu-toggle");
-        const navLinks = document.getElementById("nav-links");
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" placeholder="Votre mot de passe" required>
 
-        toggle.addEventListener("click", () => {
-            navLinks.classList.toggle("active");
-        });
-    </script>
+        <button type="submit">Connexion</button>
+        </section>
+</div>
+<script>
+    const toggle = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    toggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+</script>
 
 
-    <footer>
-        <p>Politique de confidentialité - Conditions générales d’utilisation</p>
-        <p>&copy;2026 Aurora Hair Salon. Tous droits réservés.</p>
-    </footer>
+<footer>
+    <p>Politique de confidentialité - Conditions générales d’utilisation</p>
+    <p>&copy;2026 Aurora Hair Salon. Tous droits réservés.</p>
+</footer>
 
 </body>
 
